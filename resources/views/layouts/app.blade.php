42678d01d4b3a7af2f736df92867d9cf5966fe6c
Rado Brglez <rado.brglez@gmail.com>
2019-11-12 12:18:42 +0100

    checkbox light/dark

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,7 @@
           <meta charset="utf-8">
           <meta name="viewport" content="width=device-width, initial-scale=1">
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
-          <!-- bootstrap zaradi accordiona: -->
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     </head>
     <body>
         <div id='app'>
@@ -25,6 +21,12 @@
                 </div>
 
                 <div class="navbar-end">
+                    <div class="theme-switch">
+                      <label class="checkbox">
+                          <input type="checkbox">
+                          Enable dark mode
+                      </label>
+                    </div>
                     @guest
                           <div class="navbar-item">
                             <div class="buttons">
@@ -67,7 +69,19 @@
         </main>
    
         <script src="js/app.js"></script>
-        <!--
-    -->
+        <script >
+          const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+          function switchTheme(e) {
+              console.log('switchTheme',e);
+              if (e.target.checked) {
+                  document.documentElement.setAttribute('data-theme', 'dark');
+              }
+              else {
+                  document.documentElement.setAttribute('data-theme', 'light');
+              }    
+          }
+          toggleSwitch.addEventListener('change', switchTheme, false);
+        </script>
     </body>
 </html>
